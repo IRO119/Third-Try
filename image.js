7eb19b4778333db71b9e4c80562db689dd7789e4
IRO119 <ivaniro911@live.com>
2022-05-24 16:18:25 -0600

    second commit

---
+++ image.js
@@ -0,0 +1,51 @@
+const Canvas = require("canvas");
+
+const Discord = require("discord.js")
+
+const background = "https://imgur.com/5m77wkj"
+
+const dim = {
+    height: 265,
+    width: 1200,
+    margin: 50
+}
+
+const av = {
+    size: 256,
+    x: 480,
+    y: 170
+}
+
+const generateImage = async (member) =>{
+    let username = member.user.username;
+    let discrim = member.user.discriminator;
+    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size});
+
+    const canvas = Canvas.createCanvas(dim.width, dim.height)
+    const ctx = canvas.getContext("2d")
+    
+    //draws the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+
+    //draws black tinted box
+    ctx.fillStyle = "rgba(0, 0, 0, 0, 0.8)"
+    ctx.fillRect(dim.margin, dim.margin, dim.margin - 2 * dim.margin, dim.height - 2 * dim.margin)
+
+    const avimg = await Canvas.loadImage(avatarURL)
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.clip()
+
+    ctx.drawImage(avimg, av.x, av.y)
+
+    ctx.restore()
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    return attachment
+}
+
+module.exports = generateImage
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("Discord.js");
 
-require("dotenv".config());
+require("dotenv").config();
+
+const generateImage = require("./image")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 });
 
@@ -17,6 +20,17 @@ client.on("messageCreate", (Message) => {
     if(Message.content == "hi"){
         Message.reply("Hello world!");
     }
+});
+
+const welcomeChannelId = "978465393175830568"
+
+client.on("guildMemberAdd", async (member) =>{
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+        
 });
 
 client.login(process.env.TOKEN);
