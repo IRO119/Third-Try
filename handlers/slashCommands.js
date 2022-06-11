9d653c48b4f300c9d06f4f432f3652af60d92a05
IRO119 <ivaniro911@live.com>
2022-06-11 14:55:36 +0900

    Fourth commit

---
+++ handlers/slashCommands.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+
+const getFiles = (path, ending) => {
+    return fs.readSync(path).filter(f => f.endsWith(ending))
+}
+
+module.exports = (bot, reload) => {
+    const {client} = bot
+
+    let slashCommands = getFiles("./slashCommands/", ".js")
+
+    if(slashCommands.length === 0){
+        console.log("No such slash commands loaded")
+    }
+
+    slashCommands.forEach(f => {
+        if (reload) delete require.cache[require.resolve(`../slashCommands/${f}`)]
+        
+        const slashCmnd = require(`../slashCommands/${f}`)
+        client.slashCommands.set(slashCmnd.name, slashCmnd)
+        
+    })
+}
